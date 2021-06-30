5d4df7dc83750b6e04405ff15a9bea15e2b3f372
evelisedias <evelisemacieldias@outlook.com>
2021-06-30 11:06:32 -0300

    aula4

---
+++ 4_Variaveis/js/scripts.js
@@ -0,0 +1,45 @@
+var teste = 1
+
+var nome = "Maria"
+
+console.log(teste)
+
+teste = "Evelise"
+
+console.log(teste)
+
+console.log(nome)
+
+var $nome = 'ASD';
+
+var _nome = 'ASD2';
+
+console.log($nome)
+
+console.log(_nome)
+
+var nome5 = 'Dias'
+
+console.log(nome5)
+
+var meuPrimeiroNome = "Evelise" //camelcase
+
+console.log(meuPrimeiroNome)
+
+var meusobrenome = "Dias"
+
+console.log(meusobrenome)
+
+
+
+let testando = 1;
+const ola = 2;
+
+console.log (testando)
+console.log (ola)
+
+var meuNome;
+
+meuNome = "Evelise"
+
+console.log (meuNome)
