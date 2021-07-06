c81ddc02bfe89de80d6b7577d42aacad4bd6e00a
Dias <evelise.dias@thomsonreuters.com>
2021-07-06 17:00:11 -0300

    atualizacao-aulas

---
+++ 8_null_undefined/js/scripts.js
@@ -0,0 +1,12 @@
+//Hoisting
+
+console.log(sobrenome)
+
+var nome = null;
+var sobrenome = "Dias"
+
+console.log(nome);
+console.log(sobrenome);
+nome = "Evelise";
+
+console.log(nome);
---
+++ 9_objetos/js/scripts.js
@@ -0,0 +1,23 @@
+var obj = {
+    nome: "Evelise",
+    idade: 24,
+    Profissao: "Tecnico de Suporte"
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+console.log(obj.nome)
+console.log(obj.idade)
+console.log(obj.Profissao)
+
+console.log("Meu nome é" + " " + obj.nome)
+
+obj.nome = "Dias"
+
+console.log(obj.nome)
+console.log(obj)
+
+obj.graduacao = true
+
+console.log(obj)
