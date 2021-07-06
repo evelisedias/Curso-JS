a963c9a3dd0a005fe7b4acf6052fdd72d12b3cc5
Dias <evelise.dias@thomsonreuters.com>
2021-07-06 16:00:18 -0300

    Aula7

---
+++ 6_String/js/scripts.js
@@ -0,0 +1,28 @@
+var nome = "Evelise";
+var sobrenome = "Dias"
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "ASD");
+
+
+console.log(nomeCompleto);
+
+console.log (typeof "5.29")
+
+var frase = 'está é uma frase completa'
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase) ;
+
+document.write('Ele disse: "Ola"');
+document.write("Ele disse: 'Ola'");
+
+console.log("Este numero: " + nome);
+
+
